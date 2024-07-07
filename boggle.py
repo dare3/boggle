9ad1e9407442700889d5e9030b665001ffb84a50
Dare3 <enitan_segun@yahoo.co.uk>
2024-07-06 20:35:20 -0400

    file modified

--- boggle.py
+++ boggle.py
@@ -20,14 +20,11 @@ class Boggle():
 
     def make_board(self):
         """Make and return a random boggle board."""
-
         board = []
         vowels = 'AEIOU' # ADD MORE VOWELS
-
         for y in range(5):
             row = [choice(string.ascii_uppercase) for i in range(5)] + [choice(vowels) for i in range(5)]
             board.append(row)# adding random words row to board
-
         return board
 
     def check_valid_word(self, board, word):
